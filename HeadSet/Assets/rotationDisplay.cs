aa381926e642fc42b1defa8a583e2092e59ad415
Vincent Lee <vincentlee377@hotmail.com>
2018-10-10 15:43:33 -0400

    added rotation script

---
+++ HeadSet/Assets/rotationDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rotationDisplay : MonoBehaviour {
+    public Text textElement;
+    public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textElement.text = camera.transform.localEulerAngles.ToString() ;
+	}
+
+}
