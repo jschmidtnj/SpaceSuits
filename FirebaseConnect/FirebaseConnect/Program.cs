0123f603fa5eede6af21a396065cf59b4edf2751
jschmidtnj <joshuanschmidt@gmail.com>
2018-10-17 17:14:03 -0400

    added revised firebase connect script with taskdata and suitdata syncing properly

--- FirebaseConnect/FirebaseConnect/Program.cs
+++ FirebaseConnect/FirebaseConnect/Program.cs
@@ -15,7 +15,7 @@ namespace FirebaseConnect
         public static Dictionary<string, string> LoadJson(string configfile)
         {
             Dictionary<string, string> items;
-            using (StreamReader r = new StreamReader("config.json"))
+            using (StreamReader r = new StreamReader(configfile))
             {
                 string json = r.ReadToEnd();
                 //Console.WriteLine(json);
@@ -24,9 +24,16 @@ namespace FirebaseConnect
             return items;
         }
 
+        public class TaskObj
+        {
+            public string data { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            var config = LoadJson("config.json");
+            var currentdirectory = System.IO.Directory.GetCurrentDirectory();
+            Console.WriteLine(currentdirectory);
+            var config = LoadJson(currentdirectory + "/config.json");
             var url = config["DatabaseUrl"];
             var secret = config["DatabaseAppSecret"];
             var firebase = new FirebaseClient(
@@ -41,19 +48,75 @@ namespace FirebaseConnect
             .AsObservable<string>()
             .Subscribe(d =>
             {
-                Console.WriteLine(d.Key);
-                Console.WriteLine(d.Object);
+                //Console.WriteLine(d.Key);
+                //Console.WriteLine(d.Object);
+                switch (d.Key)
+                {
+                    case "accelx":
+                        //update accelx
+                        Console.WriteLine("update accelx: " + d.Object);
+                        break;
+                    case "accely":
+                        //update accely
+                        Console.WriteLine("update accely: " + d.Object);
+                        break;
+                    case "accelz":
+                        //update accelz
+                        Console.WriteLine("update accelz: " + d.Object);
+                        break;
+                    case "battery":
+                        //update battery
+                        Console.WriteLine("update battery: " + d.Object);
+                        break;
+                    case "heartrate":
+                        //update heartrate
+                        Console.WriteLine("update heartrate: " + d.Object);
+                        break;
+                    case "moisture":
+                        //update moisture
+                        Console.WriteLine("update moisture: " + d.Object);
+                        break;
+                    case "pitch":
+                        //update pitch
+                        Console.WriteLine("update pitch: " + d.Object);
+                        break;
+                    case "primaryo2":
+                        //update primaryo2
+                        Console.WriteLine("update primaryo2: " + d.Object);
+                        break;
+                    case "roll":
+                        //update roll
+                        Console.WriteLine("update roll: " + d.Object);
+                        break;
+                    case "secondaryo2":
+                        //update secondaryo2
+                        Console.WriteLine("update secondaryo2: " + d.Object);
+                        break;
+                    case "yaw":
+                        //update yaw
+                        Console.WriteLine("update yaw: " + d.Object);
+                        break;
+                }
+
             });
-            Console.ReadKey();
             //tasks:
-            var tasks = firebase
+            dynamic taskdata = new Newtonsoft.Json.Linq.JObject();
+            var gettasks = firebase
             .Child("tasks")
             .AsObservable<string>()
-            .Subscribe(d =>
+            .Subscribe(tasks =>
             {
-                Console.WriteLine(d.Key);
-                Console.WriteLine(d.Object);
+                //Console.WriteLine(tasks.Key);
+                //Console.WriteLine(tasks.Object);
+                if (tasks.Key == "data")
+                {
+                    Console.WriteLine("update tasks: ");
+                    taskdata = Newtonsoft.Json.Linq.JObject.Parse(tasks.Object);
+                    Console.WriteLine(taskdata);
+                }
             });
+
+
             Console.ReadKey();
         }
     }
